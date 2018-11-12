fce82d59576ae793c5a46402b9bc1cfa7082b1c7
0918nobita <nobita.0918@gmail.com>
2018-11-13 01:30:14 +0900

    :sparkles: 型レベル自然数の足し算を実装した

--- index.ts
+++ index.ts
@@ -20,6 +20,28 @@ type False = 'f';
 type True = 't';
 type Bool = True | False;
 
+type Zero = { isZero: True };
+type Nat = Zero | { isZero: False, pred: Nat };
+type Succ<N extends Nat> = { isZero: False, pred: N };
+
+type Pred<N extends Succ<Nat>> = N['pred'];
+type IsZero<N extends Nat> = N['isZero'];
+
+type Sum<L extends Nat, R extends Nat>
+  = L extends Succ<infer T> ? { isZero: False, pred: Sum<T, Succ<R>>['pred'] } : R;
+
+type _0 = Zero;        // { isZero: True }
+type _1 = Succ<_0>;    // { isZero: False, pred: Zero }
+type _2 = Succ<_1>;    // { isZero: False, pred: Succ<Zero> }
+type _3 = Succ<_2>;    // { isZero: False, pred: Succ<Succ<Zero>> }
+
+type _4 = Sum<_3, _1>; // { isZero: False, pred: Succ<Succ<Succ<Zero>>> }
+type _5 = Sum<_2, _3>; // { isZero: False, pred: Succ<Succ<Succ<Succ<Zero>>>> }
+
+type f = IsZero<Zero>;      // a: True
+type g = IsZero<Succ<Zero>>; // b: False
+type h = Pred<Succ<Succ<Zero>>>;  // c: { isZero: False, pred: Zero }
+
 type And<L extends Bool, R extends Bool>
   = L extends False ? False : (R extends False ? False : True);
 
@@ -55,34 +77,3 @@ type If<Cond extends Bool, Then, Else> = { 't': Then, 'f': Else }[Cond];
 
 type d = If<True, 1, 2>;  // j: 1
 type e = If<False, 3, 4>; // k: 4
-
-type Zero = { isZero: True, pred: Nat };
-type Nat = Zero | { isZero: False, pred: Nat };
-type Succ<N extends Nat> = { isZero: False, pred: N };
-
-type Pred<N extends Succ<Nat>> = N['pred'];
-type IsZero<N extends Nat> = N['isZero'];
-
-type f = IsZero<Zero>;      // a: True
-type g = IsZero<Succ<Zero>>; // b: False
-type h = Pred<Succ<Succ<Zero>>>;  // c: { isZero: False, pred: Zero }
-
-type _0 = Zero;
-type _1 = Succ<_0>;
-type _2 = Succ<_1>;
-type _3 = Succ<_2>;
-type _4 = Succ<_3>;
-type _5 = Succ<_4>;
-
-type Plus1<N extends Nat> = { isZero: False; pred: Succ<N>; };
-type Plus2<N extends Nat> = { isZero: False; pred: Succ<Succ<N>>; };
-type Plus3<N extends Nat> = { isZero: False; pred: Succ<Succ<Succ<N>>>; };
-type Plus4<N extends Nat> = { isZero: False; pred: Succ<Succ<Succ<Succ<N>>>>; };
-
-type _6 = Plus4<_2>;
-
-// 未実装
-type Add<L extends Nat, R extends Nat>
-  = L extends Zero ? R : (R extends Zero ? L : { isZero: False, pred: R['pred'] });
-
-type _7 = Add<_5, _2>; // 正しく動作していない
