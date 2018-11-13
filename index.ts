ae2c7cbebf0d29e070f3ed4f35dc87c5c3192b4d
0918nobita <nobita.0918@gmail.com>
2018-11-13 22:10:37 +0900

    :sparkles: ISucc の修正

--- index.ts
+++ index.ts
@@ -52,13 +52,40 @@ type IPositive = NonZero & { isNegative: False };
 type INegative = NonZero & { isNegative: True };
 type Integer = INegative | IZero | IPositive;
 
-type ISucc<I extends Integer>
-  = { isZero: False, isNegative: I['isNegative'], pred: I };
-
 type IMinus<I extends Integer>
   = I extends INegative | IPositive
     ? { isZero: I['isZero'], isNegative: Not<I['isNegative']>, pred: I['pred'] }
     : IZero;
+
+type ISucc<I extends Integer>
+  = I extends IPositive | IZero
+    ? { isZero: False, isNegative: False, pred: Succ<I>['pred'] }
+    : (I extends Succ<Succ<infer T>>
+      ? { isZero: False, isNegative: True, pred: T }
+      : IZero);
+
+type _i1 = ISucc<IZero>;
+type _i2 = ISucc<_i1>;
+type _i3 = ISucc<_i2>;
+type _im3 = IMinus<_i3>;
+type _im2 = ISucc<_im3>;
+
+/*
+type ISucc<I extends Integer>
+  = { isZero: False, isNegative: I['isNegative'], pred: I };
+*/
+
+type _im1 = Succ<Zero> & { isNegative: True };
+
+/*
+type ISucc<I extends Integer>
+  = I extends IPositive | IZero
+    ? { isZero: False, isNegative: False, pred: Succ<I>['pred'] }
+    : {
+      isZero: (I extends _im1 ? True : False),
+      isNegative: (I extends _im1 ? False : True),
+      pred: (I extends _im1 ? Zero : { isZero: False, isNegative: True, pred: Pred<I> })
+    };
 
 type IPPSum<L extends IPositive | IZero, R extends IPositive | IZero>
   = L extends Succ<infer T>
@@ -71,20 +98,33 @@ type INNSum<L extends INegative, R extends INegative>
         { isZero: False, isNegative: False, pred: L['pred'] },
         { isZero: False, isNegative: False, pred: R['pred'] }>>;
 
-/*
-type IPNSum<L extends IPositive | IZero, R extends INegative>
-  = L extends ;
+type IPred<I extends Integer>
+  = I extends IPositive
+    ? Pred<I>
+    : (I extends INegative
+      ? ISucc<I>
+      : IMinus<ISucc<IZero>>);
+*/
 
+/*
+type IPNSum<L extends IPositive, R extends INegative>
+  = R extends IMinus<ISucc<infer T>> ? { isZero: Bool, isNegative: Bool, pred: T } : L ;
+*/
+
+/*
 type INPSum<L extends INegative, R extends IPositive | IZero>
   = ;
 */
 
+/*
 type _i1 = ISucc<IZero>;        //  1
 type _i2 = ISucc<_i1>;          //  2
-type _i3 = IPPSum<_i1, _i2>;    //  3
-type _im1 = IMinus<_i1>;        // -1
+type _i3 = ISucc<_i2>;          //  3
+type _i5 = IPPSum<_i2, _i3>;    //  5
 type _im2 = ISucc<_im1>;        // -2
 type _im3 = INNSum<_im1, _im2>; // -3
+type __i2 = IPNSum<_i5, _im3>;  // -2
+*/
 
 type Pred<N extends Succ<Nat>> = N['pred'];
 type IsZero<N extends Nat> = N['isZero'];
@@ -128,3 +168,17 @@ type If<Cond extends Bool, Then, Else> = { 't': Then, 'f': Else }[Cond];
 
 type d = If<True, 1, 2>;  // j: 1
 type e = If<False, 3, 4>; // k: 4
+
+type S = Atom | { car: S, cdr: S };
+type Atom = T | Nil;
+type T = True | Number;
+type Nil = False;
+type Number = Integer;
+type Cons<Car extends S, Cdr extends S> = { car: Car, cdr: Cdr };
+type List<Tuple extends {}> = Tuple;
+
+type L1 = List<[T]>;         // = { car: T, cdr: Nil }
+type L2 = List<[T, T]>;      // = { car: T, cdr: { car: T, cdr: Nil } }
+type L3 = List<[T, Nil, T]>; // = { car: T, cdr: { car: T, cdr: { car: T, cdr: Nil } } }
+
+// type S1 = Cons<ISucc<IZero>, Cons<IZero, Nil>>;
